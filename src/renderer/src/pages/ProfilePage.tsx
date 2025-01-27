2d84f6f2393f78f5d399d8e9f216429817631830
rivo pelu <dusty.emit@finestudio.org>
2025-01-28 01:51:45 +0700

    FEAT:add button logout in profile user

--- src/renderer/src/pages/ProfilePage.tsx
+++ src/renderer/src/pages/ProfilePage.tsx
@@ -1,7 +1,9 @@
 import { PageContainer } from '@renderer/components/PageContainer';
 import { CardBody, MainCard } from '@renderer/components/MainCard';
 import { useProfilePage } from '@renderer/pages/useProfilePage';
-import { Avatar } from '@mui/material';
+import { Avatar, Button } from '@mui/material';
+import { t } from 'i18next';
+import { MdLogout } from 'react-icons/md';
 
 export function ProfilePage() {
   const page = useProfilePage();
@@ -14,9 +16,19 @@ export function ProfilePage() {
               <div className={'flex  justify-between'}>
                 <div className={'flex gap-5'}>
                   <Avatar sx={{ height: 100, width: 100, borderRadius: 2 }} src={page?.profile?.data?.avatar} />
-                  <div>
-                    <h1 className={'font-semibold text-3xl capitalize'}>{page.profile?.data?.full_name}</h1>
-                    <p>{page?.profile?.data?.email}</p>
+                  <div className={'flex justify-between h-full gap-2 flex-col'}>
+                    <div>
+                      <h1 className={'font-semibold text-3xl capitalize'}>{page.profile?.data?.full_name}</h1>
+                      <p>{page?.profile?.data?.email}</p>
+                    </div>
+                    <Button
+                      onClick={() => page.onLogout()}
+                      variant={'contained'}
+                      color={'error'}
+                      startIcon={<MdLogout />}
+                    >
+                      {t('logout')}
+                    </Button>
                   </div>
                 </div>
                 <div>{page.profile?.data?.role || '-'}</div>
--- src/renderer/src/pages/useProfilePage.ts
+++ src/renderer/src/pages/useProfilePage.ts
@@ -1,11 +1,16 @@
 import { IAccountSlice } from '@renderer/redux/reducers/account.reducer';
 import { useAppSelector } from '@renderer/redux/store';
+import AuthServices from '@renderer/service/auth.service';
 
 export function useProfilePage() {
   const Account: IAccountSlice = useAppSelector((state) => state.Account);
   const profile = Account?.getMe;
+  const authService = new AuthServices();
+
+  const onLogout = authService.logout;
 
   return {
+    onLogout,
     profile,
   };
 }
